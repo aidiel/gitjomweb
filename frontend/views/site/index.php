3fba9cc526ef1d28c9c6ac0fd2409b582aa71f0d
Aidiel <aidiel.effendy@gmail.com>
2015-08-08 11:13:23 +0800

    - Change wordings Selamat Datang

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -7,9 +7,9 @@ $this->title = 'GITJOMWEB';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>GIT - JOMWEB!</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">Selamat Datang Ke Bengkel Git.</p>
 
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>
